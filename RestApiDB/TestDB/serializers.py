85357b9a2d3ece18fa140ac9c6d15660bef71bbb
PRESNOVODNOE <aaromachev1990@gmail.com>
2024-02-01 17:13:13 +0300

    Отредактировал serializers

--- RestApiDB/TestDB/serializers.py
+++ RestApiDB/TestDB/serializers.py
@@ -1,9 +1,72 @@
 from rest_framework.serializers import ModelSerializer
 
-from .models import NetworkDevice
+from .models import NetworkDevice, Interface, TrafficData, ErrorLog, UserActivity, BandwidthUsage, DeviceConfiguration
+from .models import AuthenticationLog, PerformanceMetrics, NetworkEvents, IPAddress, Configuration
 
+class ErrorLogSerializer(ModelSerializer):
+    class Meta:
+        model = ErrorLog
+        fields = '__all__'
+
+class UserActivitySerializer(ModelSerializer):
+    class Meta:
+        model = UserActivity
+        fields = '__all__'
+
+class BandwidthUsageSerializer(ModelSerializer):
+    class Meta:
+        model = BandwidthUsage
+        fields = '__all__'
+
+class DeviceConfigurationSerializer(ModelSerializer):
+    class Meta:
+        model = DeviceConfiguration
+        fields = '__all__'
+
+class AuthenticationLogSerializer(ModelSerializer):
+    class Meta:
+        model = AuthenticationLog
+        fields = '__all__'
+
+class PerformanceMetricsSerializer(ModelSerializer):
+    class Meta:
+        model = PerformanceMetrics
+        fields = '__all__'
+
+class TrafficDataSerializer(ModelSerializer):
+    class Meta:
+        model = TrafficData
+        fields = '__all__'
+
+class NetworkEventsSerializer(ModelSerializer):
+    class Meta:
+        model = NetworkEvents
+        fields = '__all__'
+
+class IPAddressSerializer(ModelSerializer):
+    class Meta:
+        model = IPAddress
+        fields = '__all__'
+
+class ConfigurationSerializer(ModelSerializer):
+    class Meta:
+        model = Configuration
+        fields = '__all__'
+
+class InterfaceSerializer(ModelSerializer):
+    traffic_data = TrafficDataSerializer(many=True, read_only=True)
+    bandwidth_usage = BandwidthUsageSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Interface
+        fields = ['id', 'name', 'status', 'traffic_data', 'bandwidth_usage']
 
 class NetworkDeviceSerializer(ModelSerializer):
+    interfaces = InterfaceSerializer(many=True, read_only=True, source='interface_set')
+    error_logs = ErrorLogSerializer(many=True, read_only=True)
+    user_activities = UserActivitySerializer(many=True, read_only=True)
+    device_configurations = DeviceConfigurationSerializer(many=True, read_only=True)
+
     class Meta:
         model = NetworkDevice
-        fields = '__all__'
+        fields = ['id', 'name', 'device_type', 'location', 'interfaces', 'error_logs', 'user_activities', 'device_configurations']
